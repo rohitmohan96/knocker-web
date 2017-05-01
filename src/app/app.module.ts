d3175d95813b80d410c45da96a899b374c7d70ab
Pranav Sriram <pranavsriram8@gmail.com>
2017-05-01 13:39:40 +0530

    did everything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AngularFireModule, AuthMethods, AuthProviders} from 'angularfire2';
+import {FilterComponent} from './filter/filter.component';
+import {HomeComponent} from './home/home.component';
+import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {Route, RouterModule} from '@angular/router';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+const config = {
+  apiKey: 'AIzaSyB5BLz_gBdc2RTpLxngsSlTuxiVk5r6sBo',
+  authDomain: 'knocker-3a330.firebaseapp.com',
+  databaseURL: 'https://knocker-3a330.firebaseio.com',
+  projectId: 'knocker-3a330',
+  storageBucket: 'knocker-3a330.appspot.com',
+  messagingSenderId: '726197767436'
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Popup
+};
+
+const appRoutes: Route[] = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'jobSearch',
+    component: FilterComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(config, firebaseAuthConfig),
+    PaginationModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/job.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class JobService {
+  url = 'http://localhost:5000/Jobs';
+
+  constructor(private http: Http) {
+  }
+
+  filterJobs(page: number = 1, locations?: string[], experience?: number, category?: string) {
+    let where = {};
+    let projection;
+    let sort;
+
+    if (locations) {
+      where['$text'] = {
+        $search: locations.join(' ')
+      };
+      projection = {
+        score: {
+          $meta: "textScore"
+        }
+      };
+
+      sort = '[("score", {"$meta": "textScore"})]';
+    }
+
+    if (experience) {
+      where['experience'] = {
+        $in: [experience - 1, experience, experience + 1]
+      };
+    }
+
+    if (category) {
+      where['categories'] = category;
+    }
+
+
+    return this.http
+      .get(this.url, {
+        search: {
+          where: where,
+          page: page
+        }
+      })
+      .toPromise()
+      .then(res => {
+        return res.json();
+      });
+  }
+}
