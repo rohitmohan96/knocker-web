919162a2f85b49d52f046410ebadef0c71ee62c4
Pranav Sriram <pranavsriram8@gmail.com>
2017-05-05 21:51:54 +0530

    made minor changes to job components

--- src/app/job-details/job-details.component.ts
+++ src/app/job-details/job-details.component.ts
@@ -28,10 +28,27 @@ export class JobDetailsComponent implements OnInit {
   };
 
 
+  urlMap = {
+    'https://jobopenings.infosys.com': 'https://upload.wikimedia.org/wikipedia/commons/9/95/Infosys_logo.svg',
+    'https://jobs.sap.com': 'https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg',
+    'https://akamaijobs.referrals.selectminds.com/': 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Akamai_logo.svg',
+    'https://jobs.capgemini.com': 'https://upload.wikimedia.org/wikipedia/commons/9/93/Logo_Capgemini.png',
+    'https://jobs.cisco.com': 'https://upload.wikimedia.org/wikipedia/commons/6/64/Cisco_logo.svg'
+  };
+
+
   ngOnInit() {
     const jobId = this.route.snapshot.params['id'];
 
     this.job = this.jobService.getJob(jobId);
+  }
+  getImage(url: string) {
+    for (const key in this.urlMap) {
+      if (url.startsWith(key)) {
+        return this.urlMap[key];
+      }
+    }
+    return '';
   }
 
 }
