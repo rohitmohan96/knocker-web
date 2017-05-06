062f0b084f11e88b27fa9079cde80928c5c9b601
Rohit Mohan <rohitmohan96@gmail.com>
2017-05-06 22:08:35 +0530

    Added wishlist in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,35 +14,22 @@ export class HomeComponent implements OnInit {
   jobs: Promise<any>;
   user: firebase.User;
   uid: string;
-  map = {
-    'C': 'red',
-    'C++': 'blue',
-    'LINUX': 'black',
-    'PYTHON': 'orange',
-    'JAVA': 'green',
-    'SQL': 'chocolate',
-    'JAVASCRIPT': 'grey',
-    'CSS': 'brown',
-    'HTML': 'black',
-    'C#': 'black'
-  };
+  crawlId: number;
 
-  imageMap = {
-    'Infosys Ltd.': 'https://upload.wikimedia.org/wikipedia/commons/9/95/Infosys_logo.svg',
-    'SAP Software Solutions': 'https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg',
-    'Akamai Technologies': 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Akamai_logo.svg',
-    'Capgemini': 'https://upload.wikimedia.org/wikipedia/commons/9/93/Logo_Capgemini.png',
-    'Cisco Systems, Inc.': 'https://upload.wikimedia.org/wikipedia/commons/6/64/Cisco_logo.svg'
-  };
-
-  constructor(private afAuth: AngularFireAuth, private jobService: JobService) {
+  constructor(private afAuth: AngularFireAuth,
+              private jobService: JobService) {
   }
 
   ngOnInit() {
-    this.afAuth.authState.subscribe(user => this.user = user);
+    this.afAuth.authState.subscribe(user => {
+      this.user = user;
+      this.uid = user.uid;
+      this.jobs = this.jobService.getPinnedJobs(this.uid);
 
-    this.uid = this.user.uid;
-    this.jobs = this.jobService.getPinnedJobs(this.uid);
+      this.jobService
+        .getCrawlId()
+        .then((crawlId) => this.crawlId = crawlId);
+    });
   }
 
 }
